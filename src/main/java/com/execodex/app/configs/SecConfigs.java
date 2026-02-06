81ef0fb37b21b1030fec2f1401bd75c3b3685da4
moldo <44651279+avrabie@users.noreply.github.com>
2026-02-06 13:24:08 +0100

    pitstop at least the uploads work

--- src/main/java/com/execodex/app/configs/SecConfigs.java
+++ src/main/java/com/execodex/app/configs/SecConfigs.java
@@ -25,10 +25,13 @@ public class SecConfigs {
                 .authorizeExchange(exchage -> exchage
                         .pathMatchers("/actuator/**").permitAll()
                         .pathMatchers("/api/greetings").permitAll()
-                        .pathMatchers("/api/minio/*").permitAll()
-                        .pathMatchers("/minio/*").permitAll()
+                        .pathMatchers("/api/bff/me").permitAll()
+                        .pathMatchers("/greetings").permitAll()
+                        .pathMatchers("/api/minio/**").permitAll()
+                        .pathMatchers("/minio/**").permitAll()
                         .anyExchange().authenticated()
                 )
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt
                         .jwtAuthenticationConverter(jwtAuthenticationConverter())));
         return http.build();
@@ -48,6 +51,12 @@ public class SecConfigs {
                             authorities.add(new SimpleGrantedAuthority("ROLE_" + role))
                     );
                 }
+            }
+            List<String> topLevelRoles = jwt.getClaim("roles");
+            if (topLevelRoles != null) {
+                topLevelRoles.forEach(role ->
+                        authorities.add(new SimpleGrantedAuthority("ROLE_" + role))
+                );
             }
             Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
             if (resourceAccess != null) {
